3e2ae502012ef689e0844440a674bc3c6b421d0b
bcabanes <b.cabanes@novactive.com>
2014-04-26 17:19:20 -0400

    add frontcow logo

--- app/index.js
+++ app/index.js
@@ -82,7 +82,7 @@ FrontCow.prototype.app = function app() {
     this.mkdir('app/fonts');
     this.mkdir('app/medias');
         this.mkdir('app/medias/images');
-        this.copy('medias/images/frontcow.jpg', 'medias/images/frontcow.jpg');
+        this.copy('medias/images/frontcow.jpg', 'app/medias/images/frontcow.jpg');
         this.mkdir('app/medias/tmp');
     this.mkdir('app/js');
         this.mkdir('app/js/modules');
