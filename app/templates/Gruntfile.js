415e2c4a144774db349a1d7d8b5cad32f1ec82aa
bcabanes <contact@benjamincabanes.com>
2014-08-30 18:26:33 -0400

    develop: enhance the gruntfile

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -1,176 +1,301 @@
 'use strict';
 
+/**
+ * Get and display all interfaces and their IP
+ */
+console.info('Interface list:');
+var os = require('os');
+var ifaces = os.networkInterfaces();
+var availableIP = [];
+for (var dev in ifaces) {
+  var alias=0;
+  ifaces[dev].forEach(function(details){
+    if (details.family === 'IPv4') {
+      availableIP[dev] = details.address;
+console.info('----- '+dev+(alias?':'+alias:''), details.address);
+      ++alias;
+    }
+  });
+}
+
+/**
+ * General IP to use
+ */
+var myIP = availableIP.en0;
+
+/**
+ * GRUNTFILE
+ */
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
 
-        sass: {
-            options: {
-                includePaths: [
-                    <% if (bourbon) { %>'app/bower_components/bourbon/dist',<% } %>
-                    <% if (fontAwesome) { %>'app/bower_components/font-awesome/scss',<% } %>
-                    'app/bower_components/foundation/scss'
-                ],
-                outputStyle: 'compressed' // 'nested' (default), 'expanded', 'compact', 'compressed'
-                // sourceMap: 'app.css.map'
-                // sourceComments: 'map',
-            },
-            dist: {
-                files: {
-                    'app/css/main.css': 'app/scss/main.scss'
-                }
-            }
-        },
+  /**
+  * Load grunt tasks automatically
+  * Read the dependencies/devDependencies/peerDependencies in the package.json
+  * and load grunt tasks that match the provided patterns.
+  */
+  require('load-grunt-tasks')(grunt, {
+    // pattern: 'grunt-*',
+    config: 'package.json',
+    scope: ['dependencies', 'devDependencies']
+  });
 
-        jshint: {
-            options: {
-                reporter: require('jshint-stylish'),
-                jshintrc: '.jshintrc'
-            },
-            all: [
-                'Gruntfile.js',
-                'app/js/{,*/}*.js'
-            ]
-        },
+  /**
+   * Time how long tasks take.
+   * Can help when optimizing build times
+   */
+  require('time-grunt')(grunt);
 
-        clean: {
-            dist: {
-                src: ['dist/*']
-            },
-            tmp: {
-                src: ['.tmp']
-            }
-        },
-        copy: {
-            fontawesome: {
-              files:[
-                {
-                  cwd:'app/bower_components/font-awesome/fonts/',
-                  expand: true,
-                  flatten: true,
-                  src: '**',
-                  dest: 'app/fonts/'
-                }
-              ]
-            },
-            dist: {
-                files: [
-                    {
-                        expand: true,
-                        cwd:'app/',
-                        src: ['css/**', 'js/**', '!js/modules/**', '!images/**', 'fonts/**', '**/*.html', '**/*.svg', '**/*.ico', '!**/*.scss', '!bower_components/**'],
-                        dest: 'dist/'
-                    }<% if (fontAwesome) { %>,{
-                        expand: true,
-                        flatten: true,
-                        src: ['app/bower_components/font-awesome/fonts/**'],
-                        dest: 'dist/fonts/',
-                        filter: 'isFile'
-                    }<% } %>
-                ]
-            }
-        },
+  /**
+   * Define the configuration for all tasks
+   */
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        imagemin: {
-          dynamic: {
-            options: {
-              optimizationLevel: 3
-            },
-            files: [{
-              expand: true,
-              cwd: 'app/medias/',
-              src: ['**/*.{png,jpg,gif,ico}'],
-              dest: 'dist/medias/'
-            }]
-          }
-        },
-
-        useminPrepare: {
-            html: 'app/*.html',
-            options: {
-                staging: '.tmp',
-                dest: 'dist'
-            }
-        },
-
-        usemin: {
-            html: ['dist/*.html'],
-            css: ['dist/css/*.css'],
-            options: {
-                dirs: ['dist']
-            }
-        },
-
-        uglify: {
-            options: {
-                mangle: true
-            }
-        },
-
-        watch: {
-            grunt: {
-                files: ['Gruntfile.js'],
-                tasks: ['sass']
-            },
-            sass: {
-                files: ['app/scss/**/*.scss'],
-                tasks: ['sass']
-            },
-            livereload: {
-                files: ['app/*.html', 'app/js/{,**/}*.js', 'app/css/{,*/}*.css', 'app/images/{,*/}*.{jpg,gif,svg,jpeg,png}'],
-                options: {
-                    livereload: true
-                }
-            }
-        },
-
-        connect: {
-            app: {
-                options: {
-                    port: 9000,
-                    base: 'app/',
-                    livereload: true
-                }
-            },
-            dist: {
-                options: {
-                    port: 9001,
-                    base: 'dist/',
-                    keepalive: true
-                }
-            }
-        },
-        open: {
-            app: {
-                path: 'http://127.0.0.1:9000/',
-                app: 'Google Chrome'
-            },
-            dist: {
-                path: 'http://127.0.0.1:9001/',
-                app: 'Google Chrome'
-            }
+    /**
+     * Watch files for changes and runs tasks based on the changed files
+     * @type {Object}
+     */
+    watch: {
+      grunt: {
+        files: ['Gruntfile.js'],
+        tasks: ['sass']
+      },
+      sass: {
+        files: ['app/scss/**/*.scss'],
+        tasks: ['sass']
+      },
+      livereload: {
+        files: ['app/*.html', 'app/js/{,**/}*.js', 'app/css/{,*/}*.css', 'app/images/{,*/}*.{jpg,gif,svg,jpeg,png}'],
+        options: {
+          livereload: true
         }
+      }
+    },
 
-    });
+  /**
+   * Grunt server settings
+   */
+  connect: {
+    options: {
+      hostname: myIP,
+      livereload: 35729,
+      port: 9000,
+      protocol: 'http' // Protocol used
+    },
+    livereload: {
+      options: {
+        open: true,
+        base: 'app'
+      }
+    },
+    dist: {
+      options: {
+        open: true,
+        base: 'dist',
+        livereload: false
+      }
+    }
+  },
 
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-usemin');
-    grunt.loadNpmTasks('grunt-open');
+    /**
+     * Empties folders to start fresh
+     */
+     clean: {
+       dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            'dist/*',
+            '!dist/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
 
-    grunt.registerTask('build', ['sass']);
-    grunt.registerTask('default', ['build', 'copy:fontawesome', 'connect:app', 'open:app', 'watch']);
+    /**
+     * C LIBSASS
+     * Compiles Sass to CSS and generates necessary files if requested
+     * @type {Object}
+     */
+    sass: {
+      options: {
+        includePaths: [
+          <% if (bourbon) { %>'app/bower_components/bourbon/dist',<% } %>
+          <% if (fontAwesome) { %>'app/bower_components/font-awesome/scss',<% } %>
+          'app/bower_components/foundation/scss'
+        ],
+        outputStyle: 'compressed', // 'nested' (default), 'expanded', 'compact', 'compressed'
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'app/css/main.css': 'app/scss/main.scss'
+        }
+      }
+    },
 
-    grunt.registerTask('livereload', ['connect:app', 'watch:livereload']);
-    grunt.registerTask('validate-js', ['jshint']);
-    grunt.registerTask('server-dist', ['connect:dist', 'open:dist']);
-    grunt.registerTask('publish', ['clean', 'copy:dist', 'useminPrepare', 'concat', 'uglify', 'usemin', 'imagemin', 'clean:tmp']);
+    /**
+     * ImageMin
+     * Image compression
+     * @type {Object}
+     */
+    imagemin: {
+      dynamic: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'app/medias/',
+            src: ['**/*.{png,jpg,gif,ico}'],
+            dest: 'dist/medias/'
+          }
+        ]
+      }
+    },
+
+    /**
+     * USEMIN PREPARE
+     * @type {Object}
+     */
+    useminPrepare: {
+      html: 'app/index.html',
+      options: {
+        staging: '.tmp',
+        dest: 'dist'
+      }
+    },
+    usemin: {
+      html: ['dist/**/*.html'],
+      css: ['dist/css/**/*.css'],
+      options: {
+        dirs: 'dist'
+      }
+    },
+
+    /**
+     * Concat (use by usemin/useminPrepare)
+     * @type {Object}
+     */
+    concat: {
+      options: {
+        separator: ';',
+        sourceMap: false,
+        sourceMapName: undefined,
+        sourceMapStyle: 'embed'
+      }
+    },
+
+    /**
+     * UglifyJS (use by usemin/useminPrepare)
+     * @type {Object}
+     */
+    uglify: {
+      options: {
+        beautify: false,
+        mangle: true
+      }
+    },
+
+    /**
+     * Copies remaining files to places other tasks can use
+     * fontawesome: copy the fontawesome font into the dist directory
+     * dist: simple frontend distribution
+     * @type {Object}
+     */
+    copy: {
+      <% if (fontAwesome) { %>
+      fontawesome: {
+        files:[
+          {
+            cwd:'app/bower_components/font-awesome/fonts/',
+            expand: true,
+            flatten: true,
+            src: '**',
+            dest: 'app/fonts/'
+          }
+        ]
+      },
+      <% } %>
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd:'app/',
+            src: ['css/**', 'js/**', '!js/modules/**', '!images/**', 'fonts/**', '**/*.html', '**/*.svg', '**/*.ico', '!**/*.scss', '!bower_components/**'],
+            dest: 'dist/'
+          }
+          <% if (fontAwesome) { %>,{
+            expand: true,
+            flatten: true,
+            src: ['app/bower_components/font-awesome/fonts/**'],
+            dest: 'dist/fonts/',
+            filter: 'isFile'
+          }
+          <% } %>
+        ]
+      }
+    },
+
+    /**
+     * jshint
+     * Check the integrity design of code
+     * @type {Object}
+     */
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish'),
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'app/js/{,*/}*.js'
+      ]
+    }
+
+  });// END grunt.initConfig
+
+  /**
+   * SERVE TASK
+   * Initializing the watch, copying styles and start compass.
+   * Lunch the local server and open the app in the browser.
+   */
+  grunt.registerTask('server', function (target) {
+
+    // $ grunt server:dist
+    if(target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'copy:fontawesome',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+  /**
+   * BUILD TASK
+   */
+  grunt.registerTask('build', [
+    'clean:dist',
+    'copy:dist',
+    'useminPrepare',
+    'usemin',
+    'concat',
+    'uglify',
+    'imagemin',
+    'clean:server'
+  ]);
+
+  /**
+   * SCAN-JS
+   * Scan the js with JSHINT
+   */
+  grunt.registerTask('scan-js', ['jshint']);
 
 };
